8994fbd78c37c1d81e7baf6b203c558efa71e011
Yoav Ram <yoavram+github@gmail.com>
2013-04-23 09:16:12 +0300

    used to run multiple jobs

---
+++ stochastic/run.sh
@@ -0,0 +1,67 @@
+#!/bin/bash  
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=1 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=2 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=5 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=7 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=10 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=15 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=20 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=35 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=50 simulation.sge
+
+
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=1 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=2 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=5 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=7 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=10 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=15 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=20 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=35 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=50 simulation.sge
+
+
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=1 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=2 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=5 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=7 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=10 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=15 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=20 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=35 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=50 simulation.sge
+
+
+
+
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=1 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=2 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=5 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=7 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=10 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=15 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=20 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=35 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=50 simulation.sge
+
+
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=1 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=2 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=5 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=7 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=10 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=15 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=20 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=35 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=50 simulation.sge
+
+
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=1 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=2 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=5 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=7 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=10 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=15 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=20 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=35 simulation.sge
+qsub -N fixation -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=50 simulation.sge
