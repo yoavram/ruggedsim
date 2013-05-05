167a1780bf0bef534a8d2e33ef02fa640f1b2bc4
Yoav Ram <yoavram+github@gmail.com>
2013-05-05 10:12:08 +0300

    pop 1e8

--- stochastic/run.sh
+++ stochastic/run.sh
@@ -1,79 +1,79 @@
 #!/bin/bash  
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=1 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=2 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=5 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=7 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=10 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=15 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=20 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=28 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=35 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=42 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=1 -v tau=50 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=1 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=2 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=5 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=7 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=10 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=15 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=20 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=28 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=35 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=42 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=1 -v tau=50 simulation.sge
 
 
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=1 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=2 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=5 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=7 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=10 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=15 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=20 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=28 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=35 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=42 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=1 -v tau=50 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=1 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=2 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=5 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=7 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=10 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=15 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=20 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=28 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=35 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=42 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=1 -v tau=50 simulation.sge
 
 
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=1 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=2 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=5 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=7 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=10 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=15 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=20 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=28 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=35 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=42 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=1 -v tau=50 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=1 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=2 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=5 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=7 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=10 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=15 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=20 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=28 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=35 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=42 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=1 -v tau=50 simulation.sge
 
 
 
 
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=1 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=2 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=5 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=7 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=10 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=15 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=20 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=28 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=35 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=42 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.01 -v pi=10 -v tau=50 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=1 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=2 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=5 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=7 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=10 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=15 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=20 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=28 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=35 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=42 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.01 -v pi=10 -v tau=50 simulation.sge
 
 
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=1 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=2 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=5 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=7 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=10 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=15 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=20 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=28 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=35 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=42 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.05 -v pi=10 -v tau=50 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=1 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=2 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=5 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=7 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=10 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=15 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=20 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=28 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=35 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=42 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.05 -v pi=10 -v tau=50 simulation.sge
 
 
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=1 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=2 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=5 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=7 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=10 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=15 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=20 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=28 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=35 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=42 simulation.sge
-qsub -t 1-10 -v pop_size=1000000 -v s=0.1 -v pi=10 -v tau=50 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=1 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=2 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=5 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=7 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=10 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=15 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=20 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=28 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=35 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=42 simulation.sge
+qsub -t 1-10 -v pop_size=100000000 -v s=0.1 -v pi=10 -v tau=50 simulation.sge
