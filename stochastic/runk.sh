01b1040df4a6b9c0b07a6d7a50cdabb6fd1b4c46
Yoav Ram <yoavram+github@gmail.com>
2013-11-25 15:15:29 +0200

    run simulations with simk

---
+++ stochastic/runk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash  
+qsub -t 1-10 -v pi=-10 -v tau=1 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=2 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=5 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=7 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=10 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=15 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=20 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=28 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=35 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=42 simulation2.sge
+qsub -t 1-10 -v pi=-10 -v tau=50 simulation2.sge
+
+
+qsub -t 1-10 -v pi=-1 -v tau=1 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=2 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=5 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=7 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=10 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=15 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=20 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=28 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=35 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=42 simulation2.sge
+qsub -t 1-10 -v pi=-1 -v tau=50 simulation2.sge
+
+
+qsub -t 1-10 -v pi=-0.1 -v tau=1 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=2 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=5 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=7 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=10 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=15 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=20 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=28 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=35 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=42 simulation2.sge
+qsub -t 1-10 -v pi=-0.1 -v tau=50 simulation2.sge
